421258d6ba7770ca6cbb0d3d7368b2559dac605f
Marisel <shenapam3@gmail.com>
2024-12-02 09:40:17 +0300

    Bentley

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "i2QkRK5B#KWlSYTUu2WxCtDGUhxiNdEo3er3Gv9A4dV4rKClJu6s",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
